a8d6cf0025cb3d80c2e8080644e50d8cbda41a95
Crystian Lefundes <crystian.lf@gmail.com>
2021-07-06 19:58:01 -0300

    Create a file with a data about books used in this project

---
+++ database.js
@@ -0,0 +1,58 @@
+const livros = [
+    {
+        id: 1,
+        autor: "Digital minimalism",
+        nome: "Cal Newport",
+        categoria: "Produtividade",
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 2,
+        autor: "O Genocídio do negro brasileiro",
+        nome: "Abdias do Nascimento",
+        categoria: "História brasileira",
+        paginas: 254,
+        recomenda: false,
+        leu: true
+    },
+    {
+        id: 3,
+        autor: "As veias abertas da América Latina",
+        nome: "Eduardo Galeano",
+        categoria: "Américas",
+        paginas: 400,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 4,
+        autor: "Algoritmos para viver",
+        nome: "Brian Christian",
+        categoria: "Tecnologia",
+        paginas: 412,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 5,
+        autor: "Thinking, fast and slow",
+        nome: "Daniel Kahneman",
+        categoria: "Estilo de vida",
+        paginas: 418,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 6,
+        autor: "Padrões Javascript",
+        nome: "Stoyan Stefanov",
+        categoria: "Tecnologia",
+        paginas: 231,
+        recomenda: true,
+        leu: true
+    },
+]
+
+module.exports = livros
